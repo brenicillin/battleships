8923ea735149d7eaf31a3fc70dfb763840d0eaf7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-16 18:58:10 -0500

    feat: add cell class initialize

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -13,34 +13,36 @@ RSpec.describe do
     it 'cell has coordinates' do
       cell = Cell.new("B4")
 
-      expect(cell.coordinate).to be_a("B4")
+      expect(cell.coordinate).to eq("B4")
     end
     
     it 'cell has no ships' do
       cell = Cell.new("B4")
 
-      expect(cell.ship).to be_a(nil)
+      expect(cell.ship).to eq(nil)
     end
   end
   describe '#cell can hold ships?' do
     it 'cell is empty' do
       cell = Cell.new("B4")
 
-      expect(cell.empty?).to be_a(true)
+      expect(cell.empty?).to eq(true)
     end
     
     it 'A ship can be placed' do
-    cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser)
+      cruiser = Ship.new("Cruiser", 3)
+      cell = Cell.new("B4")
+      cell.place_ship(cruiser)
     
-      expect(cell.ship).to eq(Ship)
+      expect(cell.ship).to be_a(Ship)
     end
     
     it 'cell is no longer empty' do
       cruiser = Ship.new("Cruiser", 3)
+      cell = Cell.new("B4")
       cell.place_ship(cruiser)
 
-      expect(cell.ship).to eq(Ship)
+      expect(cell.ship).to be_a(Ship)
       expect(cell.empty?).to eq(false)
     end
   end
