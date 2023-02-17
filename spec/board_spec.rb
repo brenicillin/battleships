64dab476a00c9fcaff1b1d0bce3177cacce7be00
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-17 17:32:08 -0500

    Create valid placement method and write more tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,8 +31,21 @@ RSpec.describe Board do
       cruiser = Ship.new('Cruiser', 3)
       submarine = Ship.new('Submarine', 2)
 
-      expect(board.valid_placement?(cruiser, ['A1', 'A2'])).to eq('false')
-      expect(board.valid_placement?(submarine, ['A2', 'A3', 'A4'])).to eq('false')
+      expect(board.valid_placement?(cruiser, ['A1', 'A2'])).to eq(false)
+      expect(board.valid_placement?(submarine, ['A2', 'A3', 'A4'])).to eq(false)
     end
+    
+    it 'can tell if coordinates are consecutive' do
+      board = Board.new
+      cruiser = Ship.new('Cruiser', 3)
+      submarine = Ship.new('Submarine', 2)
+
+      expect(board.valid_placement?(cruiser, ['A1', 'A2', 'A3'])).to eq(true)
+      expect(board.valid_placement?(cruiser, ['A1', 'A2', 'A4'])).to eq(false)
+      expect(board.valid_placement?(submarine, ['A1', 'C1'])).to eq(false)
+      expect(board.valid_placement?(cruiser, ['A3', 'A2', 'A1'])).to eq(false)
+      expect(board.valid_placement?(submarine, ['C1', 'B1'])).to eq(false)
+    end
+
   end
 end
