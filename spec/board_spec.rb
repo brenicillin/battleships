0ba9d0f6e7a5868f6fa7242f3d7bd307da3dae6d
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-16 23:25:19 -0500

    test: add board.valid_coordinates tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Board do
       expect(board.cells).to be_a(Hash)
     end
   end
+
+  describe '#valid_coordinates' do
+    it 'can tell if coordinate is on board' do
+      board = Board.new
+
+      expect(board.valid_coordinate?('A1')).to eq(true)
+      expect(board.valid_coordinate?('D4')).to eq(true)
+      expect(board.valid_coordinate?('A5')).to eq(false)
+      expect(board.valid_coordinate?('E1')).to eq(false)
+      expect(board.valid_coordinate?('A22')).to eq(false)
+    end
+  end
 end
