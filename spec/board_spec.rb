c2507a932c553d7df6df86dbca5836da4bb10ee3
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-17 17:13:15 -0500

    test: add valid_placement tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,5 +25,14 @@ RSpec.describe Board do
       expect(board.valid_coordinate?('E1')).to eq(false)
       expect(board.valid_coordinate?('A22')).to eq(false)
     end
+
+    it 'can tell if placement is valid' do
+      board = Board.new
+      cruiser = Ship.new('Cruiser', 3)
+      submarine = Ship.new('Submarine', 2)
+
+      expect(board.valid_placement?(cruiser, ['A1', 'A2'])).to eq('false')
+      expect(board.valid_placement?(submarine, ['A2', 'A3', 'A4'])).to eq('false')
+    end
   end
 end
