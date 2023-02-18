7243c9bb9d022d17e4dfed3a2b6f57629fa3859d
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-18 10:56:40 -0500

    test: add game setup tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,14 @@
 class Game
   def initialize
-
+    
   end
 
   def start
     welcome = 'Welcome to BATTLESHIP\nEnter p to play. Enter q to quit'
+    p welcome
+  # input = gets.chomp
+  # if input.upcase = 'P'
+  # game.setup
+  # else "Until next time..."
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,9 +9,28 @@ RSpec.describe Game do
     end
 
     it 'has welcome message' do
-    game = Game.new
+      game = Game.new
 
-    expect(game.start).to eq('Welcome to BATTLESHIP\nEnter p to play. Enter q to quit')
+      expect(game.start).to eq('Welcome to BATTLESHIP\nEnter p to play. Enter q to quit')
+    end
+  end
+
+  describe '#setup' do
+    it 'creates cpu ships and board' do
+      game = Game.new
+
+      expect(@cpu_board).to be_a(Board)
+      expect(@cpu_submarine).to be_a(Ship)
+      expect(@cpu_cruiser).to be_a(Ship)
+    end
+  
+
+    it 'creates player ships and board' do
+      game = Game.new
+
+      expect(@player_board).to be_a(Board)
+      expect(@player_cruiser).to be_a(Ship)
+      expect(@player_submarine).to be_a(Ship)
     end
   end
 end
