d21812b64a18dfe6d1961a3232d910e3b81c0971
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-20 21:51:57 -0500

    Make helper methods for render and refactor

--- lib/board.rb
+++ lib/board.rb
@@ -23,7 +23,7 @@ class Board
   end
 
   def valid_coordinate?(coordinate)
-    @cells.has_key?(coordinate)
+    @cells.key?(coordinate)
   end
 
   def valid_placement?(ship, ship_coordinates)
@@ -40,7 +40,7 @@ class Board
   def place(ship, ship_coordinates)
     if valid_placement?(ship, ship_coordinates) && validate_cell(ship_coordinates)
       ship_coordinates.each do |cell|
-        self.cells[cell].place_ship(ship)
+        cells[cell].place_ship(ship)
       end
     else false
     end
@@ -48,76 +48,98 @@ class Board
 
   def render(view = false)
     if view == true
-      "  1 2 3 4 \n" +
-      "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)}\n" +
-      "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)}\n" +
-      "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)}\n" +
-      "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)}"
-      else
-      "  1 2 3 4 \n" +
-      "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}\n" +
-      "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}\n" +
-      "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}\n" +
-      "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}"
-    
-     end
+      rendered_true
+    else
+      rendered
+    end
   end
+  # def render(view = false)
+  #   if view == true
+  #     "  1 2 3 4 \n" +
+  #     "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)}\n" +
+  #     "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)}\n" +
+  #     "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)}\n" +
+  #     "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)}"
+  #     else
+  #     "  1 2 3 4 \n" +
+  #     "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}\n" +
+  #     "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}\n" +
+  #     "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}\n" +
+  #     "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}"
+  #    end
+  # end
 
   #helper methods
+  
+  def rendered_true
+    "  1 2 3 4 \n" +
+    "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)}\n" +
+    "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)}\n" +
+    "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)}\n" +
+    "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)}"
+  end
+
+  def rendered
+    "  1 2 3 4 \n" +
+    "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}\n" +
+    "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}\n" +
+    "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}\n" +
+    "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}"
+  end
 
   def validate_cell(ship_coordinates)
     ship_coordinates.all? do |cell|
-      self.cells[cell].empty?
+      cells[cell].empty?
     end
   end
 
   def valid_cruiser_placements 
-    valid_placements_c = [
-    %w(A1 B1 C1),
-    %w(B1 C1 D1),
-    %w(A2 B2 C2),
-    %w(B2 C2 D2),
-    %w(A3 B3 C3),
-    %w(B3 C3 D3),
-    %w(A4 B4 C4),
-    %w(B4 C4 D4),
-    %w(A1 A2 A3),
-    %w(A2 A3 A4),
-    %w(B1 B2 B3),
-    %w(B2 B3 B4),
-    %w(C1 C2 C3),
-    %w(C2 C3 C4),
-    %w(D1 D2 D3),
-    %w(D2 D3 D4)
+    [
+      %w(A1 B1 C1),
+      %w(B1 C1 D1),
+      %w(A2 B2 C2),
+      %w(B2 C2 D2),
+      %w(A3 B3 C3),
+      %w(B3 C3 D3),
+      %w(A4 B4 C4),
+      %w(B4 C4 D4),
+      %w(A1 A2 A3),
+      %w(A2 A3 A4),
+      %w(B1 B2 B3),
+      %w(B2 B3 B4),
+      %w(C1 C2 C3),
+      %w(C2 C3 C4),
+      %w(D1 D2 D3),
+      %w(D2 D3 D4)
     ]
   end
-  
-  def valid_submarine_placements 
-    valid_placements_s = [
-    %w(A1 B1),
-    %w(B1 C1),
-    %w(C1 D1),
-    %w(A2 B2),
-    %w(B2 C2),
-    %w(C2 D2),
-    %w(A3 B3),
-    %w(B3 C3),
-    %w(C3 D3),
-    %w(A4 B4),
-    %w(B4 C4),
-    %w(C4 D4),
-    %w(A1 A2),
-    %w(A2 A3),
-    %w(A3 A4),
-    %w(B1 B2),
-    %w(B2 B3),
-    %w(B3 B4),
-    %w(C1 C2),
-    %w(C2 C3),
-    %w(C3 C4),
-    %w(D1 D2),
-    %w(D2 D3),
-    %w(D3 D4)
+
+  def valid_submarine_placements
+    [
+      %w(A1 B1),
+      %w(B1 C1),
+      %w(C1 D1),
+      %w(A2 B2),
+      %w(B2 C2),
+      %w(C2 D2),
+      %w(A3 B3),
+      %w(B3 C3),
+      %w(C3 D3),
+      %w(A4 B4),
+      %w(B4 C4),
+      %w(C4 D4),
+      %w(A1 A2),
+      %w(A2 A3),
+      %w(A3 A4),
+      %w(B1 B2),
+      %w(B2 B3),
+      %w(B3 B4),
+      %w(C1 C2),
+      %w(C2 C3),
+      %w(C3 C4),
+      %w(D1 D2),
+      %w(D2 D3),
+      %w(D3 D4)
     ]
   end
 end
